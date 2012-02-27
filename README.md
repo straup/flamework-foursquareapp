0b6e569c26141316a55a4baadf77431a587c190d
straup <straup@gmail.com>
2012-02-27 18:50:50 +0100

    fussing with copy; remove scary notice from README

--- README.md
+++ README.md
@@ -3,6 +3,3 @@ flamework-foursquareapp
 
 Drop-in code and libraries to use foursquare as a single-sign-on validation
 service for Flamework projects.
-
-**This code is not ready to use yet** unless you're already familiar with flamework and are willing to accept that some of the templates are still a bit wonky.
-
--- bin/setup.sh
+++ bin/setup.sh
@@ -2,26 +2,26 @@
 
 WHOAMI=`readlink -f $0`
 WHEREAMI=`dirname $WHOAMI`
-FOURSQUAREAPP=`dirname $WHEREAMI`
+FOURSQUARE_APP=`dirname $WHEREAMI`
 
 PROJECT=$1
 
 echo "copying application files to ${PROJECT}"
-cp ${FOURSQUAREAPP}/www/*.php ${PROJECT}/www/
+cp ${FOURSQUARE_APP}/www/*.php ${PROJECT}/www/
 
 # echo "copying templates to ${PROJECT}"
-cp ${FOURSQUAREAPP}/www/templates/*.txt ${PROJECT}/www/templates/
+cp ${FOURSQUARE_APP}/www/templates/*.txt ${PROJECT}/www/templates/
 
 echo "copying library code to ${PROJECT}"
-cp ${FOURSQUAREAPP}/www/include/*.php ${PROJECT}/www/include/
+cp ${FOURSQUARE_APP}/www/include/*.php ${PROJECT}/www/include/
 
 echo "copying database schemas to ${PROJECT}; you will still need to run database alters manually"
 
 YMD=`date "+%Y%m%d"`
 mkdir ${PROJECT}/schema/alters
 
-cat ${FOURSQUAREAPP}/schema/db_main.schema >> ${PROJECT}/schema/db_main.schema
-cat ${FOURSQUAREAPP}/schema/db_main.schema >> ${PROJECT}/schema/alters/${YMD}.db_main.schema
+cat ${FOURSQUARE_APP}/schema/db_main.schema >> ${PROJECT}/schema/db_main.schema
+cat ${FOURSQUARE_APP}/schema/db_main.schema >> ${PROJECT}/schema/alters/${YMD}.db_main.schema
 
 echo "setup (mostly) complete"
 echo "you will still need to update your config file manually"
@@ -29,8 +29,20 @@ echo ""
 
 # TO DO: generate oauth cookie secret
 
-cat ${FOURSQUAREAPP}/www/include/config.php.example
+echo "Things you'll need to add to your config.php file:"
+echo ""
+
+cat ${FOURSQUARE_APP}/www/include/config.php.example
+
+echo "--------------------------------------------"
+echo ""
 
 # TO DO: .htaccess configs
 
-cat ${FOURSQUAREAPP}/www/.htaccess
+echo "Things you'll need to add to your .htaccess file:"
+echo ""
+
+cat ${FOURSQUARE_APP}/www/.htaccess
+
+echo "--------------------------------------------"
+echo ""
